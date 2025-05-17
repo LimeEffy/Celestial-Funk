0c7e095820d22ae29a9ad91e089da132bd49453e
Fajar Tegar Nugraha <morrisalexey@gmail.com>
2025-05-17 20:23:51 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,8 +4,29 @@
 --- PREFIX: lep
 --- MOD_AUTHOR: [Lime_Effy]
 --- MOD_DESCRIPTION: Funk up your Planets
---- VERSION: 1.0.0
+--- VERSION: 1.0.2
 --- DEPENDENCIES: [malverk]
+
+AltTexture({
+  key = 'celestial_tex',
+  set = 'Booster',
+  path = 'boosters.png',
+  keys = {
+    'p_celestial_normal_1',
+    'p_celestial_normal_2',
+    'p_celestial_normal_3',
+    'p_celestial_normal_4',
+    'p_celestial_jumbo_1',
+    'p_celestial_jumbo_2',
+    'p_celestial_mega_1',
+    'p_celestial_mega_2'
+  },
+  original_sheet = 'true',
+  display_pos = 'p_celestial_jumbo_1',
+  loc_txt = {
+    name = 'Celestial Funky Pack'
+  }
+})
 
 AltTexture({
     key = 'planets_tex',
@@ -42,33 +63,13 @@ AltTexture({
     name = 'Funky Black Hole'
   }
 })
-AltTexture({
-  key = 'celestial_tex',
-  set = 'Booster',
-  path = 'boosters.png',
-  keys = {
-    'p_celestial_normal_1',
-    'p_celestial_normal_2',
-    'p_celestial_normal_3',
-    'p_celestial_normal_4',
-    'p_celestial_jumbo_1',
-    'p_celestial_jumbo_2',
-    'p_celestial_mega_1',
-    'p_celestial_mega_2'
-  },
-  original_sheet = 'true',
-  display_pos = 'p_celestial_jumbo_1',
-  loc_txt = {
-    name = 'Celestial Funky Pack'
-  }
-})
 
 TexturePack({
     key = 'lep_planets_pack',
     textures = {
+      'lep_celestial_tex',
       'lep_planets_tex',
       'lep_black_hole_tex',
-      'lep_celestial_tex',
     },
     loc_txt = {
       name = 'Celestial Funk',
